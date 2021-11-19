5405ffb6476c7f6754e635bea0ea4d4a9e711d35
keszigabesz <91908624+keszigabesz@users.noreply.github.com>
2021-11-19 11:20:17 +0100

    kész végre valahára

--- js-3-feladatok-matching-game/js/main.js
+++ js-3-feladatok-matching-game/js/main.js
@@ -8,48 +8,92 @@ const dealCards = () => {
         classList.remove("random");
     }
 }
-
 const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
-let foundPairs=0;
+let foundPairs = 0;
 
-function flipCard() {
-    this.classList.add('flip');
-    this.classList.add('active');    
+let firstFlip = true;
+
+const gameEvaluation = () => {
     const activeCards = document.querySelectorAll('.active');
     if (activeCards.length === 2) {
         if (activeCards[0].textContent === activeCards[1].textContent) {
             activeCards[0].classList.remove('active');
             activeCards[1].classList.remove('active');
             foundPairs++;
-            if (foundPairs===5) {
+            if (foundPairs === 5) {
+                timerStop();
                 setTimeout(() => {
                     const cardFrontArray = document.querySelectorAll(".card__front");
                     for (let i = 0; i < cards.length; i++) {
                         console.log(i);
                         console.log(cards[i]);
-                        foundPairs=0;
-                        cards[i].classList.remove('flip');                        
+                        foundPairs = 0;
+                        cards[i].classList.remove('flip');
                         cardFrontArray[i].classList.add('random');
-                    }  
+                        timerContainer.textContent = '00:00';
+                    }
                     setTimeout(() => {
                         dealCards();
-                    }, 1000);             
-                }, 4000);
+                    }, 1000);
+                }, 3000);
             }
         } else {
             setTimeout(() => {
                 activeCards[0].classList.remove('active');
                 activeCards[1].classList.remove('active');
-                activeCards[0].classList.remove('flip');                
-                activeCards[1].classList.remove('flip');                
+                activeCards[0].classList.remove('flip');
+                activeCards[1].classList.remove('flip');
             }, 800);
-        }        
+        }
     }
 }
 
-export {
-    dealCards
+function flipCard() {
+    this.classList.add('flip');
+    this.classList.add('active');
+    if (firstFlip) {
+        timerStart();
+        firstFlip = false;
+    }
+    gameEvaluation();
 }
+
+let elapsedTime = new Date;
+elapsedTime = elapsedTime.getTime();
+elapsedTime = 0;
+const timerOptions = {
+    minute: '2-digit',
+    second: '2-digit'
+};
+let timerID;
+const timerContainer = document.querySelector("#timer");
+
+function timerStart() {
+    timerContainer.textContent = '00:00';
+    timerID = setInterval(timer, 1000);
+    
+    function timer() {
+        elapsedTime += 1000;
+        let timeInMillisec = new Date;
+        timeInMillisec.setTime(elapsedTime);
+        const formattedTime = timeInMillisec.toLocaleTimeString('hu', timerOptions);
+        timerContainer.textContent = formattedTime;
+    }
+}
+
+function timerStop() {
+    clearInterval(timerID);
+    elapsedTime = 0;
+    firstFlip = true;
+}
+
+
+
+window.addEventListener("load", dealCards);
+
+/* export {
+    dealCards,
+} */
